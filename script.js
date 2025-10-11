8508aed6cd8b564bb365743fd8986d3b984f4c2c
Balazs Farago <balu.farago@gmail.com>
2025-10-11 22:04:42 +0200

    15. DOM

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("JavaScript is connected! Let's find some destination!");
+const mainTitle = document.querySelector("#main-title");
+const highlightedText = document.querySelector("#highlight");
+const firstCard = document.querySelector(".card");
+const allCards = document.querySelectorAll(".card");
+
+console.log("The main title element:", mainTitle);
+console.log("The highlighted span element:", highlightedText);
+console.log("The first card element:", firstCard);
+console.log("A list of all the card elements:", allCards);
