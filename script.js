d36ecc8feae1615237cef1db45c5e3da4ddc69c1
Balazs Farago <balu.farago@gmail.com>
2025-10-12 23:50:07 +0200

    16. Changing Content and Styles

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const mainTitle = document.querySelector("#main-title");
 const highlightedText = document.querySelector("#highlight");
 const firstCard = document.querySelector(".card");
 const allCards = document.querySelectorAll(".card");
+const footer = document.querySelector(".footer");
 
-console.log("The main title element:", mainTitle);
-console.log("The highlighted span element:", highlightedText);
-console.log("The first card element:", firstCard);
-console.log("A list of all the card elements:", allCards);
+mainTitle.textContent = "World's Most Epic Destinations";
+highlightedText.textContent = "unforgettable";
+
+mainTitle.style.fontWeight = "bold";
+
+firstCard.style.border = "2px solid navy";
+
+footer.innerHTML =
+  '<p>&copy; 2025 Farago Destinations | <a href="#">Contact Us</a></p>';
